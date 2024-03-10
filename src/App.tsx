4225eece98188b2f72bd3342141427d54fd0676d
Sayuni Ranawaka <ranawakarasd.21@itfac.mrt.ac.lk>
2024-03-10 18:54:08 +0000

    [temporary] Refactor styling and fix typography in App.css and LandingPage.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import Signupf1 from "./pages/Signupf1";
 import Login from "./pages/Login";
+<<<<<<< HEAD
 // import DashboardPage from "./pages/DashboardPage";
 import "./App.css";
 import BarNavigation from "./components/BarNavigation";
+=======
+import DashboardPage from "./pages/DashboardPage";
+import "./App.css";
+>>>>>>> 428a0b0b ([temporary] Refactor styling and fix typography in App.css and LandingPage.tsx)
 
 // Main App component
 const App: React.FC = () => {
