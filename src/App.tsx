ae4d4f83b7746bb17c4e8367e2e3558f26e32f75
Sayuni Ranawaka <codse202f080@student.nibm.lk>
2024-03-15 02:37:18 +0530

    Refactor dependencies and remove unused code

--- src/App.tsx
+++ src/App.tsx
@@ -4,26 +4,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import Signupf1 from "./pages/Signupf1";
 import Login from "./pages/Login";
-<<<<<<< HEAD
-<<<<<<< HEAD
-// import DashboardPage from "./pages/DashboardPage";
-import "./App.css";
-import BarNavigation from "./components/BarNavigation";
-=======
-import DashboardPage from "./pages/DashboardPage";
-import "./App.css";
->>>>>>> 428a0b0b ([temporary] Refactor styling and fix typography in App.css and LandingPage.tsx)
-=======
-// import DashboardPage from "./pages/DashboardPage";
-import "./App.css";
-import BarNavigation from "./components/BarNavigation";
 
->>>>>>> 804d2c4b3775c25da31462f7a7aa1cf170b1f036
+// import DashboardPage from "./pages/DashboardPage";
+import "./App.css";
+import BarNavigation from "./components/BarNavigation";
 
 // Main App component
 const App: React.FC = () => {
 	return (
-<<<<<<< HEAD
 		<>
 			<BrowserRouter>
 				<Routes>
@@ -39,37 +27,14 @@ const App: React.FC = () => {
 						path="/login"
 						element={<Login />}
 					/>
-					<Route path='/myTestCompany' element={<LandingPage />} />		
-					
+					<Route
+						path="/myTestCompany"
+						element={<LandingPage />}
+					/>
 				</Routes>
 			</BrowserRouter>
-			
 		</>
-		
-=======
-		<BrowserRouter>
-			<Routes>
-				<Route
-					path="/"
-					element={<LandingPage />}
-				/>
-				<Route
-					path="/signup"
-					element={<Signupf1 />}
-				/>
-				<Route
-					path="/login"
-					element={<Login />}
-				/>
-				<Route
-					path="/myTestCompany"
-					element={<BarNavigation />}
-
-				/>
-			</Routes>
-		</BrowserRouter>
 	);
->>>>>>> 804d2c4b3775c25da31462f7a7aa1cf170b1f036
 };
 
 export default App;
