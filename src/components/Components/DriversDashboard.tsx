14bfb7109c4a8e41792a746e8faeb0954ca5f97f
Piranaavei Beremkumar <108848056+berem1809@users.noreply.github.com>
2024-07-04 22:01:39 +0530

    some modifications

--- src/components/Components/DriversDashboard.tsx
+++ src/components/Components/DriversDashboard.tsx
@@ -32,9 +32,9 @@ const PieChart: React.FC<PieChartProps> = ({
         borderColor: [
           'rgba(255, 99, 132, 1)',
           'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
+         
         ],
-        borderWidth: 1,
+        borderWidth: 2,
       },
     ],
   };
